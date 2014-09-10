82e268999f8f9fb0b9b86657e216a48fcc5d55f0
SeedPaths-User <lessdismissive@email.com>
2014-09-10 12:53:49 -0600

    Owen Updates

--- TriviaGame/Program.cs
+++ TriviaGame/Program.cs
@@ -19,18 +19,28 @@ namespace TriviaGame
         //This functions gets the full list of trivia questions from the Trivia.txt document
         static List<Trivia> GetTriviaList()
         {
-            //Get Contents from the file.  Remove the special char "\r".  Split on each line.  Convert to a list.
-            List<string> contents = File.ReadAllText("trivia.txt").Replace("\r", "").Split('\n').ToList();
+            bool playing = true;
 
-            //Each item in list "contents" is now one line of the Trivia.txt document.
+            while (playing)
+            {
+                Random rng = new Random();
+
+                int randomNum = rng.Next();
+            }
+
+                //Get Contents from the file.  Remove the special char "\r".  Split on each line.  Convert to a list.
+                List<string> contents = File.ReadAllText("trivia.txt").Replace("\r", "").Split('\n').ToList();
+
+                //Each item in list "contents" is now one line of the Trivia.txt document.
+
+                //make a new list to return all trivia questions
+                List<Trivia> returnList = new List<Trivia>();
+                // TODO: go through each line in contents of the trivia file and make a trivia object.
+                //       add it to our return list.
+                // Example: Trivia newTrivia = new Trivia("what is my name?*question");
+                //Return the full list of trivia questions
+                return returnList;
             
-            //make a new list to return all trivia questions
-            List<Trivia> returnList = new List<Trivia>();
-            // TODO: go through each line in contents of the trivia file and make a trivia object.
-            //       add it to our return list.
-            // Example: Trivia newTrivia = new Trivia("what is my name?*question");
-            //Return the full list of trivia questions
-            return returnList;
         }
     }
 }
