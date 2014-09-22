0e839785bf457691a35ac000197f6bbcff77e7da
SeedPaths-User <lessdismissive@email.com>
2014-09-22 13:08:40 -0600

    HighScoresUpdate

--- TriviaGame/Program.cs
+++ TriviaGame/Program.cs
@@ -42,5 +42,32 @@ namespace TriviaGame
                 return returnList;
             
         }
+        static void AddHighScore(int playerScore)
+        {
+            Console.WriteLine("Your name");
+            string playerName = Console.ReadLine();
+            OwenEntities db = new OwenEntities();
+            HighScore.newHighScore = new HighScore();
+            newHighScore.DateCreated = DateTime.Now;
+            newHighScore.Game = playerName;
+            newHighScore.Score = playerScore;
+            db.HighScores.Add(newHighScore);
+            db.SaveChanges();
+
+        }
+
+
+        static void DisplayHighScores()
+        {
+            Console.Clear();
+            Console.WriteLine("Trivia Game");
+            Console.WriteLine("------------");
+            OwenEntities db = new OwenEntities();
+            List<HighScore> highScoreList = db.HighScores.Where(x => x.Game == "Trivia Game").OrderByDescending(x => x.Score).Take(10).ToList();
+            foreach (HighScore highScore in highScoreList)
+            {
+                Console.WriteLine("{0},{1} - {2} on {3}", highScoreList.IndexOf(highScore) + 1, highScore.Name, highScore.Score);
+            }
+        }
     }
 }
